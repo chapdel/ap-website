c8d21fbc2cb5b8fd8e0bf3b18a0bc29594f37e51
chapdel <drop@chapdel.me>
2023-03-31 22:30:32 +0100

    Fixed logo

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     class="antialiased bg-white dark:bg-black text-black dark:text-white min-h-screen place-content-center flex flex-col items-center justify-center text-sm sm:text-base">
     <div class="flex-1 flex flex-col gap-y-16">
       <div class="flex flex-col gap-y-4 items-center justify-center h-screen">
-        <img src="./logo.png" class="h-60" alt="">
+        <img src="/logo.png" class="h-60" alt="">
       </div>
     </div>
   </div>
