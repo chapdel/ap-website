43de8f7846578274b8fc86f20d52af34415200dd
chapdel <drop@chapdel.me>
2023-04-01 13:37:10 +0100

    Added Menu

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div
+    class="antialiased bg-white dark:bg-black text-black dark:text-white min-h-screen place-content-center flex flex-col items-center justify-center text-sm sm:text-base">
+    <div class="flex-1 flex flex-col gap-y-16">
+      <div class="flex flex-col gap-y-4 items-center justify-center h-screen">
+        <img src="/logo.png" class="h-60" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+useHead({
+  title: "L'Agence Poulie"
+})
+</script>
