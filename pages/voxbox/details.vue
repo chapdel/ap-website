fa6abd5cc9926d0c577711f83123df7ae99b99e1
chapdel <chapdel.kamga2@gmail.com>
2023-07-20 14:10:14 +0100

    c

--- pages/voxbox/details.vue
+++ pages/voxbox/details.vue
@@ -185,7 +185,7 @@
                                                         class="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                                                         <img alt="" loading="lazy" width="32" height="32" decoding="async"
                                                             data-nimg="1" class="h-7 w-7" style="color:transparent"
-                                                            src="./media/planetaria.ecd81ade.svg">
+                                                            src="/voxbox-hero.jpg">
                                                     </div>
                                                     <dl class="flex flex-auto flex-wrap gap-x-2">
                                                         <dt class="sr-only">Company</dt>
@@ -201,7 +201,7 @@
                                                         class="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                                                         <img alt="" loading="lazy" width="32" height="32" decoding="async"
                                                             data-nimg="1" class="h-7 w-7" style="color:transparent"
-                                                            src="./media/planetaria.ecd81ade.svg">
+                                                            src="/voxbox-hero.jpg">
                                                     </div>
                                                     <dl class="flex flex-auto flex-wrap gap-x-2">
                                                         <dt class="sr-only">Company</dt>
@@ -217,7 +217,7 @@
                                                         class="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                                                         <img alt="" loading="lazy" width="32" height="32" decoding="async"
                                                             data-nimg="1" class="h-7 w-7" style="color:transparent"
-                                                            src="./media/planetaria.ecd81ade.svg">
+                                                            src="/voxbox-hero.jpg">
                                                     </div>
                                                     <dl class="flex flex-auto flex-wrap gap-x-2">
                                                         <dt class="sr-only">Company</dt>
